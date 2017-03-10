2d55298a57764e3267044a4c9ac272369a99f2fe
stefanl <stefanl@aurora-scc.int>
2017-03-10 15:25:42 +0100

    The gtf_file has been fixed to not contain an undefined column name.

--- lib/stefans_libs/file_readers/gtf_file.pm
+++ lib/stefans_libs/file_readers/gtf_file.pm
@@ -1,6 +1,6 @@
 package stefans_libs::file_readers::gtf_file;
 
-#  Copyright (C) 2016-11-02 Stefan Lang 
+#  Copyright (C) 2016-11-02 Stefan Lang
 
 #  This program is free software; you can redistribute it
 #  and/or modify it under the terms of the GNU General Public License
@@ -14,7 +14,6 @@ package stefans_libs::file_readers::gtf_file;
 
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
-
 
 use strict;
 use warnings;
@@ -45,62 +44,56 @@ Fields must be tab-separated. Also, all but the final field in each feature line
 
 
 =cut
+
 sub new {
 
-    my ( $class, $debug ) = @_;
-    my ($self);
-    $self = {
-        'debug'           => $debug,
-        'slice_length' => 5e+6,
-        'chr_path' => '',
-        'arraySorter'     => arraySorter->new(),
-        'header_position' => { 
-            'seqname' => 0,
-            'source' => 1,
-            'feature' => 2,
-            'start' => 3,
-            'end' => 4,
-            'score' => 5,
-            'strand' => 6,
-            'frame' => 7,
-            'attribute' => 8,
-        },
-        'default_value'   => [],
-        'header'          => [
-            'seqname',
-            'source',
-            'feature',
-            'start',
-            'end',
-            'score',
-            'strand',
-            'frame',
-            'attribute',       ],
-        'data'            => [],
-        'index'           => {},
-        'last_warning'    => '',
-        'subsets'         => {}
-    };
-    bless $self, $class if ( $class eq "stefans_libs::file_readers::gtf_file" );
+	my ( $class, $debug ) = @_;
+	my ($self);
+	$self = {
+		'debug'           => $debug,
+		'slice_length'    => 5e+6,
+		'chr_path'        => '',
+		'arraySorter'     => arraySorter->new(),
+		'header_position' => {
+			'seqname'   => 0,
+			'source'    => 1,
+			'feature'   => 2,
+			'start'     => 3,
+			'end'       => 4,
+			'score'     => 5,
+			'strand'    => 6,
+			'frame'     => 7,
+			'attribute' => 8,
+		},
+		'default_value' => [],
+		'header'        => [
+			'seqname', 'source', 'feature', 'start',
+			'end',     'score',  'strand',  'frame',
+			'attribute',
+		],
+		'data'         => [],
+		'index'        => {},
+		'last_warning' => '',
+		'subsets'      => {}
+	};
+	bless $self, $class if ( $class eq "stefans_libs::file_readers::gtf_file" );
 
-    return $self;
+	return $self;
 }
-
 
 ## two function you can use to modify the reading of the data.
 
-sub pre_process_array{
+sub pre_process_array {
 	my ( $self, $data ) = @_;
 	##you could remove some header entries, that are not really tagged as such...
 	my $t;
-	if ( @$data[0] =~ m/^#/ ){
-		$t = shift ( @$data );
+	if ( @$data[0] =~ m/^#/ ) {
+		$t = shift(@$data);
 		$t =~ s/^#//;
-		$self->Add_2_Description( $t );
+		$self->Add_2_Description($t);
 	}
 	return 1;
 }
-
 
 =head2 efficient_match_chr_position ( $chr, $start, $end, $max_dist )
 
@@ -113,13 +106,14 @@ sub efficient_match_chr_position {
 	$max_dist ||= 0;
 	$end ||= $start;
 	local $SIG{__WARN__} = sub { };
-	my $rep_pdl = $self ->get_pdls_4_chr( $chr, $start );
-	if (  ref($rep_pdl) eq "PDL" ) {
-		my $t1 = $rep_pdl->slice(',1') <= $end + $max_dist;
-		my $t2 = $rep_pdl->slice(',2') >= $start - $max_dist;
-		my @intron_ids = list( transpose( which( $t1 + $t2 == 2 ) ) );	
-		return $self->get_subset_4_PDL_ids($chr, $start, \@intron_ids);
-		return @{$self->{'subset_4_PDL'}->{$chr}->GetAsArray('line_id')}[@intron_ids];
+	my $rep_pdl = $self->get_pdls_4_chr( $chr, $start );
+	if ( ref($rep_pdl) eq "PDL" ) {
+		my $t1         = $rep_pdl->slice(',1') <= $end + $max_dist;
+		my $t2         = $rep_pdl->slice(',2') >= $start - $max_dist;
+		my @intron_ids = list( transpose( which( $t1 + $t2 == 2 ) ) );
+		return $self->get_subset_4_PDL_ids( $chr, $start, \@intron_ids );
+		return @{ $self->{'subset_4_PDL'}->{$chr}->GetAsArray('line_id') }
+		  [@intron_ids];
 	}
 	return ();
 }
@@ -132,200 +126,260 @@ match the chromosomal area to the own data and returns the own matching row numb
 
 sub efficient_match_chr_position_plus_one {
 	my ( $self, $chr, $start, $end, $max_dist ) = @_;
-	my @return = $self->efficient_match_chr_position( $chr, $start, $end, $max_dist );
-	if ( @return ) {
-		if ( $self->Rows == $return[@return-1] +1 ){
-			push( @return , undef)
-		}else {
-			push( @return , $return[@return-1] +1)
+	my @return =
+	  $self->efficient_match_chr_position( $chr, $start, $end, $max_dist );
+	if (@return) {
+		if ( $self->Rows == $return[ @return - 1 ] + 1 ) {
+			push( @return, undef );
+		}
+		else {
+			push( @return, $return[ @return - 1 ] + 1 );
 		}
 		return @return;
-	}else {
+	}
+	else {
 		$end ||= $start;
 		$start = $end;
-		$end = $start + 4e+6;
-		warn "I have no matches in the asked for region, therefore I try to find them here: $chr:$start-$end\n"if ( $self->{'debug'});
+		$end   = $start + 4e+6;
+		warn
+"I have no matches in the asked for region, therefore I try to find them here: $chr:$start-$end\n"
+		  if ( $self->{'debug'} );
 		@return = $self->efficient_match_chr_position( $chr, $start, $end );
 		if ( @return == 0 ) {
-			@return = $self->efficient_match_chr_position( $chr, $start, @{@{$self->{'data'}}[$self->Rows()-1]}[$self->Header_Position('end')]);
+			@return = $self->efficient_match_chr_position( $chr, $start,
+				@{ @{ $self->{'data'} }[ $self->Rows() - 1 ] }
+				  [ $self->Header_Position('end') ] );
 		}
 		return $return[0];
 	}
-	Carp::confess ( "must not reach this point\n");
+	Carp::confess("must not reach this point\n");
 }
-
 
 sub get_cDNA_4_transcript {
 	my ( $self, $id, $seqpath ) = @_;
 	$self->{'chr_path'} ||= $seqpath;
 	$seqpath ||= $self->{'chr_path'};
-	
+
 	unless ( defined $self->{'exons'} ) {
 		$self->{'exons'} = $self->_copy_without_data();
 		my $t = $self->createIndex('feature')->{'exon'};
-		unless ( ref($t) eq "ARRAY") {
-			Carp::confess( "Why is the index -> exon not an array??".root->print_perl_var_def(  $self->createIndex('feature') ). "\n");
+		unless ( ref($t) eq "ARRAY" ) {
+			Carp::confess( "Why is the index -> exon not an array??"
+				  . root->print_perl_var_def( $self->createIndex('feature') )
+				  . "\n" );
 		}
-		@{$self->{'exons'}->{'data'}} = @{$self->{'data'}}[@$t];
+		@{ $self->{'exons'}->{'data'} } = @{ $self->{'data'} }[@$t];
 	}
-	Carp::confess ( "I need a seqpath as second argument!" ) unless ( -d $seqpath) ;
-	my $ids = $self->{'exons'}->createIndex ( 'transcript_id' )->{$id };
+	Carp::confess("I need a seqpath as second argument!")
+	  unless ( -d $seqpath );
+	my $ids = $self->{'exons'}->createIndex('transcript_id')->{$id};
 	unless ( defined $ids ) {
 		warn "ID $id not found!\n";
 		return "";
 	}
 	## by definition this has to be on exactly one chromosome
-	my $chr = @{@{$self->{'exons'}->{'data'}}[@$ids[0]]}[0];
-	my $gname = @{@{$self->{'exons'}->{'data'}}[@$ids[0]]}[ $self->{'exons'}->Header_Position('gene_name')];
+	my $chr   = @{ @{ $self->{'exons'}->{'data'} }[ @$ids[0] ] }[0];
+	my $gname = @{ @{ $self->{'exons'}->{'data'} }[ @$ids[0] ] }
+	  [ $self->{'exons'}->Header_Position('gene_name') ];
 	$self->{'fastafiles'} ||= {};
-	unless ( defined  $self->{'fastafiles'}->{$chr} ){
+	unless ( defined $self->{'fastafiles'}->{$chr} ) {
 		print "read sequence from $chr\n";
 		$self->{'fastafiles'}->{$chr} = fastaFile->new();
 		if ( -f "$seqpath/$chr.fa" ) {
-			$self->{'fastafiles'}->{$chr} -> AddFile("$seqpath/$chr.fa" );
-		}elsif ( -f "$seqpath/$chr.fa.gz"){
-			$self->{'fastafiles'}->{$chr} -> AddFile("$seqpath/$chr.fa.gz" );
+			$self->{'fastafiles'}->{$chr}->AddFile("$seqpath/$chr.fa");
 		}
-		 
+		elsif ( -f "$seqpath/$chr.fa.gz" ) {
+			$self->{'fastafiles'}->{$chr}->AddFile("$seqpath/$chr.fa.gz");
+		}
+
 	}
 	my $tmp = $self->_copy_without_data();
-	push( @{$tmp->{'data'}}, @{$self->{'exons'}->{'data'}}[@$ids] );
-	$tmp = $tmp -> Sort_by( [['start', 'numeric']]);
+	push( @{ $tmp->{'data'} }, @{ $self->{'exons'}->{'data'} }[@$ids] );
+	$tmp = $tmp->Sort_by( [ [ 'start', 'numeric' ] ] );
 	my $seq = '';
-	my $ff = fastaFile->new();
-	
-	for ( my $i = 0; $i < $tmp->Lines(); $i ++ ) {
-		
-		$seq .= $self->{'fastafiles'}->{$chr} -> Get_SubSeq ( @{@{$tmp->{'data'}}[$i]}[3,4] );
+	my $ff  = fastaFile->new();
+
+	for ( my $i = 0 ; $i < $tmp->Lines() ; $i++ ) {
+
+		$seq .= $self->{'fastafiles'}->{$chr}
+		  ->Get_SubSeq( @{ @{ $tmp->{'data'} }[$i] }[ 3, 4 ] );
 	}
 	$seq = uc($seq);
-	
-	if ( @{@{$self->{'data'}}[@$ids[0]]}[6] eq "-"){
-		$seq = $ff->revComplement( $seq );
+
+	if ( @{ @{ $self->{'data'} }[ @$ids[0] ] }[6] eq "-" ) {
+		$seq = $ff->revComplement($seq);
 	}
-	$ff->Create( "$chr:$id $gname cdna right strand", $seq);
+	$ff->Create( "$chr:$id $gname cdna right strand", $seq );
 	return $ff->AsFasta();
-	
+
 }
 
 sub After_Data_read {
 	my ($self) = @_;
 	print "gtf file has been read\n";
 	## process the atribute into a set of columns
-	#gene_id "ENSG00000223972.5"; gene_type "transcribed_unprocessed_pseudogene"; gene_status "KNOWN"; gene_name "DDX11L1"; level 2; havana_gene "OTTHUMG00000000961.2";
-	my ($values, $tmp, $helper, $a,@tmp, @p);
-	$helper = data_table->new();
-	$helper -> {'string_separator'} = '"';
-	$helper -> {'line_separator'} = "[ =]";
+#gene_id "ENSG00000223972.5"; gene_type "transcribed_unprocessed_pseudogene"; gene_status "KNOWN"; gene_name "DDX11L1"; level 2; havana_gene "OTTHUMG00000000961.2";
+
+	my ( $values, $tmp, $helper, $a, @tmp, @p );
+	$helper                       = data_table->new();
+	$helper->{'string_separator'} = '"';
+	$helper->{'line_separator'}   = "[ =]";
 	my $added = {};
-	for ( my $i = 0; $i < $self->Lines(); $i ++){
-		@{@{$self->{'data'}}[$i]}[8] =~ s/;$//;
-		@tmp = split( /; ?/, @{@{$self->{'data'}}[$i]}[8]);
-		$values= ();
-		for ( $a=0; $a < @tmp; $a++ ){
+	for ( my $i = 0 ; $i < $self->Lines() ; $i++ ) {
+		@{ @{ $self->{'data'} }[$i] }[8] =~ s/;$//;
+		@tmp = split( /; ?/, @{ @{ $self->{'data'} }[$i] }[8] );
+		Carp::confess(
+			    ref($self)
+			  . "::After_Data_read -> line $1 '"
+			  . @{ @{ $self->{'data'} }[$i] }[8]
+			  . "' could not be split by /; ?/ - please adjust the pattern match\n"
+		) unless (@tmp);
+		if ( @tmp == 1 ){
+			warn "This is no gtf3 file\n";
+			last;
+		}
+		$values = ();
+		for ( $a = 0 ; $a < @tmp ; $a++ ) {
 			$tmp[$a] =~ s/"$//;
 			@p = $tmp[$a] =~ m/([_\w]*)[ =]"?(.*)/g;
-			$values ->{$p[0]} = $p[1];
-			unless ( $added->{$p[0]} ){
-				( $added->{$p[0]} ) = $self->Add_2_Header ($p[0]);
+			Carp::confess(
+				    ref($self)
+				  . "::After_Data_read -> line $1 '"
+				  . $tmp[$a]
+				  . "' could not be split by /([_\\w]*)[ =]\"?(.*)/ - please adjust the pattern match\n"
+				  . @{ @{ $self->{'data'} }[$i] }[8]
+				  . "\nThe while table line:\n"
+				  . root->print_perl_var_def( $self->get_line_asHash($i) )
+				  . ";\n" )
+			  unless ( @p > 1 );
+			$values->{ $p[0] } = $p[1];
+			unless ( $added->{ $p[0] } ) {
+				( $added->{ $p[0] } ) = $self->Add_2_Header( $p[0] );
 			}
 		}
 		foreach ( keys %$values ) {
-			@{@{$self->{'data'}}[$i]}[$added->{$_} ] = $values->{$_};
+			@{ @{ $self->{'data'} }[$i] }[ $added->{$_} ] = $values->{$_};
 		}
-		if ( $i % 10000 == 0) {
-			print "Processed line $i\n". root->print_perl_var_def( $self->{'header'}) 
-			 if ( $self->{'debug'} );
+		if ( $i % 10000 == 0 ) {
+			print "Processed line $i\n"
+			  . root->print_perl_var_def( $self->{'header'} )
+			  if ( $self->{'debug'} );
 		}
 	}
-	$self->{'__max_header__'} = scalar( @{$self->{'header'}});
-	$self->drop_column( 'attribute');
-	print "Finished loading th gtf file\n";
+	$self->{'__max_header__'} = scalar( @{ $self->{'header'} } );
+	$self->drop_column('attribute') if ( defined $self->Header_Position('attribute'));
+	$self->drop_all_indecies();
+	$self->{'subsets'} = {};
+	print "Finished loading the gtf file\n";
 	return $self;
 }
 
 sub get_chr_subID_4_start {
 	my ( $self, $start ) = @_;
-	$start ||=1;
-	return floor(($start / $self->{'slice_length'}));
+	$start ||= 1;
+	return floor( ( $start / $self->{'slice_length'} ) );
 }
 
 sub get_pdls_4_chr {
 	my ( $self, $chr, $start ) = @_;
 	my ($data);
-	$self->{'PDL'} ||={};
+	$self->{'PDL'} ||= {};
 	$self->{'PDL'}->{$chr} ||= [];
-	foreach ( keys %{$self->{'PDL'}} ) {
-		delete($self->{'PDL'}->{$_}) unless ( $_ eq $chr );
+	foreach ( keys %{ $self->{'PDL'} } ) {
+		delete( $self->{'PDL'}->{$_} ) unless ( $_ eq $chr );
 	}
 	$self->{'subset_4_PDL'} ||= {};
 	$self->{'subset_4_PDL'}->{$chr} ||= [];
-	foreach ( keys %{$self->{'subset_4_PDL'}} ) {
-		delete($self->{'subset_4_PDL'}->{$_}) unless ( $_ eq $chr );
+	foreach ( keys %{ $self->{'subset_4_PDL'} } ) {
+		delete( $self->{'subset_4_PDL'}->{$_} ) unless ( $_ eq $chr );
 	}
-	unless ( defined $self->Header_Position('line_id') ){
+	unless ( defined $self->Header_Position('line_id') ) {
+
 		#print "I define dmy own line_id:\n";
-		$self->add_column('line_id', [ 0..($self->Rows()-1)] );
-		#print "\$exp = ".root->print_perl_var_def($self->get_line_as_hash(2)).";\n";
+		$self->add_column( 'line_id', [ 0 .. ( $self->Rows() - 1 ) ] );
+
+   #print "\$exp = ".root->print_perl_var_def($self->get_line_as_hash(2)).";\n";
 	}
 	unless ( defined $self->{'subsetter'} ) {
-		my @col_ids = $self -> define_subset( 'matcher', ['seqname', 'start', 'end']);
-		$self->{'subsetter'} = stefans_libs::file_readers::bed_file ->new();
-		for ( my $i = 0; $i < $self->Lines(); $i ++) {
-			push ( @{$self->{'subsetter'}->{'data'}}, [@{@{$self->{'data'}}[$i]}[@col_ids]] );
+		my @col_ids =
+		  $self->define_subset( 'matcher', [ 'seqname', 'start', 'end' ] );
+		$self->{'subsetter'} = stefans_libs::file_readers::bed_file->new();
+		for ( my $i = 0 ; $i < $self->Lines() ; $i++ ) {
+			push(
+				@{ $self->{'subsetter'}->{'data'} },
+				[ @{ @{ $self->{'data'} }[$i] }[@col_ids] ]
+			);
 		}
-	} 
-	my $chr_id = $self->get_chr_subID_4_start( $start );
+	}
+	my $chr_id = $self->get_chr_subID_4_start($start);
+
 	#print "I got the chr_id $chr_id for the start $start\n";
 	#Carp::confess ( $self->AsString() );
-	unless ( defined @{$self->{'PDL'}->{$chr}}[$chr_id] ) {
-		my ($regions_start, $region_end );
-		$regions_start = $chr_id * $self->{'slice_length'} -10;
-		$region_end = ($chr_id+1) * $self->{'slice_length'} +10;
-		
-		print "END\nnew mapper/$chr_id for chr $chr:$regions_start-$region_end ";
-		
-		@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id] = $self->_copy_without_data();
+	unless ( defined @{ $self->{'PDL'}->{$chr} }[$chr_id] ) {
+		my ( $regions_start, $region_end );
+		$regions_start = $chr_id * $self->{'slice_length'} - 10;
+		$region_end    = ( $chr_id + 1 ) * $self->{'slice_length'} + 10;
+
+		print
+		  "END\nnew mapper/$chr_id for chr $chr:$regions_start-$region_end ";
+
+		@{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id] =
+		  $self->_copy_without_data();
+
 		#warn "I subset to $chr,$regions_start, $region_end \n";
-		@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->{'data'} = [ 
-			@{$self->{'data'}}[$self->{'subsetter'}->efficient_match_chr_position( $chr,$regions_start, $region_end )] 
+		@{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id]->{'data'} = [
+			@{ $self->{'data'} }[
+			  $self->{'subsetter'}
+			  ->efficient_match_chr_position( $chr, $regions_start,
+				  $region_end )
+			]
 		];
-		
-		#print "I create the PDL/$chr_id for chr $chr and id $chr_id and got ".@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->Lines()." lines back\n";
-		if ( @{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->Rows == 0 ){
-			@{$self->{'PDL'}->{$chr}}[$chr_id] = '';
+
+#print "I create the PDL/$chr_id for chr $chr and id $chr_id and got ".@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->Lines()." lines back\n";
+		if ( @{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id]->Rows == 0 ) {
+			@{ $self->{'PDL'}->{$chr} }[$chr_id] = '';
 			return ();
 		}
 
-		@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id] -> add_column ( 'INDEX', [0..(@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->Rows()-1)]);
-		@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id] -> define_subset ( 'PDL', [ 'INDEX','start','end', 'line_id']);
-		#print "The 'PDL' subset contains these columns:",join(", ",@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->Header_Position('PDL') )."\n";
-		#print "This is what should built up the pdl for chr $chr:\n". @{$self->{'subset_4_PDL'}->{$chr}}[$chr_id] -> GetAsObject('PDL')->AsString();
-		@{$self->{'PDL'}->{$chr}}[$chr_id] = @{$self->{'subset_4_PDL'}->{$chr}}[$chr_id] -> GetAsObject('PDL')->GetAsPDL();		
-		
-		#print "I got a object of class". ref(@{$self->{'PDL'}->{$chr}}[$chr_id]). " that can be used to locate genomic areas on $chr\n";
-	
+		@{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id]->add_column(
+			'INDEX',
+			[
+				0 .. (
+					@{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id]->Rows() - 1 )
+			]
+		);
+		@{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id]
+		  ->define_subset( 'PDL', [ 'INDEX', 'start', 'end', 'line_id' ] );
+
+#print "The 'PDL' subset contains these columns:",join(", ",@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->Header_Position('PDL') )."\n";
+#print "This is what should built up the pdl for chr $chr:\n". @{$self->{'subset_4_PDL'}->{$chr}}[$chr_id] -> GetAsObject('PDL')->AsString();
+		@{ $self->{'PDL'}->{$chr} }[$chr_id] =
+		  @{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id]->GetAsObject('PDL')
+		  ->GetAsPDL();
+
+#print "I got a object of class". ref(@{$self->{'PDL'}->{$chr}}[$chr_id]). " that can be used to locate genomic areas on $chr\n";
+
 	}
-	return @{$self->{'PDL'}->{$chr}}[$chr_id] ;
+	return @{ $self->{'PDL'}->{$chr} }[$chr_id];
 }
 
 sub read_file {
-	 my ( $self, $filename, $lines ) = @_;
-	 return undef unless ( -f $filename );
-	 if ( -f $filename.".xls" ) {
-	 	my $data_table = data_table->new();
-	 	$data_table -> read_file ( $filename.".xls" ,$lines);
-	 	$self->Add_2_Header(  $data_table->{'header'}  );
-	 	$self->{'data'} = $data_table->{'data'};
-	 }
-	 else {
-	 	$self->SUPER::read_file( $filename, $lines );
-	 }
-	 return $self;
+	my ( $self, $filename, $lines ) = @_;
+	return undef unless ( -f $filename );
+	if ( -f $filename . ".xls" ) {
+		my $data_table = data_table->new();
+		$data_table->read_file( $filename . ".xls", $lines );
+		$self->Add_2_Header( $data_table->{'header'} );
+		$self->{'data'} = $data_table->{'data'};
+	}
+	else {
+		$self->SUPER::read_file( $filename, $lines );
+	}
+	return $self;
 }
 
 sub print_as_table {
-	my ( $self,$outfile ) = @_;
+	my ( $self, $outfile ) = @_;
 	my @temp;
 	@temp = split( "/", $outfile );
 	pop(@temp);
@@ -337,7 +391,7 @@ sub print_as_table {
 	  or Carp::confess(
 		ref($self)
 		  . "::print2file -> I can not create the outfile '$outfile'\n$!\n" );
-		my $str = '';
+	my $str = '';
 	my @default_values;
 	my @line;
 	foreach my $description_line ( @{ $self->{'description'} } ) {
@@ -366,11 +420,12 @@ sub print_as_table {
 	return $outfile;
 }
 
-
 sub get_subset_4_PDL_ids {
-	my ( $self, $chr,$start, $ids ) = @_;
-	my $chr_id = $self->get_chr_subID_4_start( $start );
-	return @{@{$self->{'subset_4_PDL'}->{$chr}}[$chr_id]->GetAsArray('line_id')}[@$ids] ;
+	my ( $self, $chr, $start, $ids ) = @_;
+	my $chr_id = $self->get_chr_subID_4_start($start);
+	return
+	  @{ @{ $self->{'subset_4_PDL'}->{$chr} }[$chr_id]->GetAsArray('line_id') }
+	  [@$ids];
 }
 
 1;
--- t/stefans_libs_file_readers_1gtf_file.t
+++ t/stefans_libs_file_readers_1gtf_file.t
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 use strict;
 use warnings;
-use Test::More tests => 15;
+use Test::More tests => 17;
 BEGIN { use_ok 'stefans_libs::file_readers::gtf_file' }
 
 use FindBin;
@@ -53,7 +53,7 @@ $test_object->read_file($infile);
 $test_object->After_Data_read();
 
 $exp = {
-  'attribute' => 'gene_id "ENSG00000223972.5"; gene_type "transcribed_unprocessed_pseudogene"; gene_status "KNOWN"; gene_name "DDX11L1"; level 2; havana_gene "OTTHUMG00000000961.2"',
+ # 'attribute' => 'gene_id "ENSG00000223972.5"; gene_type "transcribed_unprocessed_pseudogene"; gene_status "KNOWN"; gene_name "DDX11L1"; level 2; havana_gene "OTTHUMG00000000961.2"',
   'end' => '14409',
   'exon_id' => undef,
   'exon_number' => undef,
@@ -88,7 +88,7 @@ is_deeply( $test_object->get_line_asHash(0),
 #print "\$exp = ".root->print_perl_var_def( $test_object->get_line_asHash( 5) ).";\n";
 
 $exp = {
-  'attribute' => 'gene_id "ENSG00000223972.5"; transcript_id "ENST00000456328.2"; gene_type "transcribed_unprocessed_pseudogene"; gene_status "KNOWN"; gene_name "DDX11L1"; transcript_type "processed_transcript"; transcript_status "KNOWN"; transcript_name "DDX11L1-002"; exon_number 3; exon_id "ENSE00002312635.1"; level 2; tag "basic"; transcript_support_level "1"; havana_gene "OTTHUMG00000000961.2"; havana_transcript "OTTHUMT00000362751.1"',
+#  'attribute' => 'gene_id "ENSG00000223972.5"; transcript_id "ENST00000456328.2"; gene_type "transcribed_unprocessed_pseudogene"; gene_status "KNOWN"; gene_name "DDX11L1"; transcript_type "processed_transcript"; transcript_status "KNOWN"; transcript_name "DDX11L1-002"; exon_number 3; exon_id "ENSE00002312635.1"; level 2; tag "basic"; transcript_support_level "1"; havana_gene "OTTHUMG00000000961.2"; havana_transcript "OTTHUMT00000362751.1"',
   'end' => '14409',
   'exon_id' => 'ENSE00002312635.1',
   'exon_number' => '3',
@@ -119,40 +119,39 @@ is_deeply( $test_object->get_line_asHash(5),
 	$exp, "a real world file has been parsed as expected (last line)" );
 
 #print "\$exp = ".root->print_perl_var_def( $test_object->{'header'} ).";\n";
-$exp = [ 'seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute', 'gene_id', 'gene_type', 'gene_status', 'gene_name', 'level', 'havana_gene', 'transcript_id', 'transcript_type', 'transcript_status', 'transcript_name', 'tag', 'transcript_support_level', 'havana_transcript', 'exon_number', 'exon_id' ];
+$exp = [ 'seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'gene_id', 'gene_type', 'gene_status', 'gene_name', 'level', 'havana_gene', 'transcript_id', 'transcript_type', 'transcript_status', 'transcript_name', 'tag', 'transcript_support_level', 'havana_transcript', 'exon_number', 'exon_id' ];
 is_deeply ( $test_object->{'header'}, $exp ,'right header info');
 
 #print "\$exp = ".root->print_perl_var_def( $test_object->{'header_position'} ).";\n";
 $exp = {
-  'attribute' => '8',
   'end' => '4',
-  'exon_id' => '23',
-  'exon_number' => '22',
+  'exon_id' => '22',
+  'exon_number' => '21',
   'feature' => '2',
   'frame' => '7',
-  'gene_id' => '9',
-  'gene_name' => '12',
-  'gene_status' => '11',
-  'gene_type' => '10',
-  'havana_gene' => '14',
-  'havana_transcript' => '21',
-  'level' => '13',
+  'gene_id' => '8',
+  'gene_name' => '11',
+  'gene_status' => '10',
+  'gene_type' => '9',
+  'havana_gene' => '13',
+  'havana_transcript' => '20',
+  'level' => '12',
   'score' => '5',
   'seqname' => '0',
   'source' => '1',
   'start' => '3',
   'strand' => '6',
-  'tag' => '19',
-  'transcript_id' => '15',
-  'transcript_name' => '18',
-  'transcript_status' => '17',
-  'transcript_support_level' => '20',
-  'transcript_type' => '16'
+  'tag' => '18',
+  'transcript_id' => '14',
+  'transcript_name' => '17',
+  'transcript_status' => '16',
+  'transcript_support_level' => '19',
+  'transcript_type' => '15'
 };
 is_deeply ( $test_object->{'header_position'}, $exp ,'right header info');
-ok( $test_object->{'__max_header__'} == 24, "internal Max_Header \$test_object ($test_object->{'__max_header__'})" );
+ok( $test_object->{'__max_header__'} == 23, "internal Max_Header \$test_object ($test_object->{'__max_header__'})" );
 
-
+my $before = $test_object->get_line_asHash(2);
 $value = $test_object->select_where('feature', sub { $_[0] eq "exon"});
 
 is_deeply(
@@ -161,12 +160,19 @@ is_deeply(
 	'select_where returns a  stefans_libs::file_readers::gtf_file'
 );
 
+is_deeply( $test_object->get_line_asHash(2), $before, "The select_where run did not modify the table structure" );
+
+#warn "\$exp = ".root->print_perl_var_def($value->get_line_asHash(0) ).";\n";
+
 is_deeply( $value->get_line_asHash(0), $test_object->get_line_asHash(2), 'dropped all but exons' );
 #$value->{'__max_header__'} = 24;
-ok( $value->{'__max_header__'} == 24, "internal Max_Header subset ($value->{'__max_header__'})" );
+ok( $value->{'__max_header__'} == 23, "internal Max_Header subset ($value->{'__max_header__'})" );
 
-print "The pdl should just not throw any errors...\n";
-print $value->get_pdls_4_chr('chr1');
+#print "The pdl should just not throw any errors...\n";
+@values = $value->get_pdls_4_chr('chr1')->unpdl();
+#print "\$exp = ".root->print_perl_var_def( \@values ).";\n";
+$exp = [ [ [ '0' ], [ '13221' ], [ '14409' ], [ '3' ] ] ];
+is_deeply( \@values, $exp, "got the right mapper pdl" );
 
 # A handy help if you do not know what you should expect
 #print "\$exp = ".root->print_perl_var_def($value ).";\n";
